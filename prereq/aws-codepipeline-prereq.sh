53c829822b014f93fe180c4755189d0a1a941e29
aaleon20 <aaleon20@outlook.com>
2021-05-07 11:44:17 -0500

    Success

--- prereq/aws-codepipeline-prereq.sh
+++ prereq/aws-codepipeline-prereq.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 aws cloudformation deploy \
---region us-east-1 \
---stack-name codepipeline-lambda-prereq \
+--region us-east-2 \
+--stack-name codepipeline-gid-prereq \
 --template-file ./aws-codepipeline-prereq.yml \
 --capabilities CAPABILITY_IAM \
 --capabilities CAPABILITY_NAMED_IAM
