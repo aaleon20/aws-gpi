fe0249141827a153ae9171360fecdf1907e9de7f
Andres Leon <aaleon20@outlook.com>
2021-05-06 23:41:29 -0500

    Success

--- lambda_function.py
+++ lambda_function.py
@@ -2,7 +2,9 @@ import boto3
 import os
 from boto3.dynamodb.conditions import Key
 
-DYNAMO_PLATZI = os.environ['DYNAMO_PLATZI']
+# Hola a todos companeros, Carlos es el mejor profe de AWS del Mundo
+
+DYNAMO_BD = os.environ['DYNAMO_BD']
 
 class DynamoAccessor:
     def __init__(self, dynamo_table):
@@ -14,7 +16,7 @@ class DynamoAccessor:
         return response["Items"][0] if any(response["Items"]) else None
 
 def lambda_handler(event, context):
-    dynamo_backend = DynamoAccessor(DYNAMO_PLATZI)
+    dynamo_backend = DynamoAccessor(DYNAMO_BD)
     db_element = dynamo_backend.get_data_from_dynamo(event['cc'])
     return db_element
 
