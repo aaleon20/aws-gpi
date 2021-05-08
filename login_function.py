0bcdbce5dd600487a532c310fb1c614e525949e5
aaleon20 <aaleon20@outlook.com>
2021-05-08 00:34:54 -0500

    Modificacion timeout lambda

--- login_function.py
+++ login_function.py
@@ -4,19 +4,16 @@ from boto3.dynamodb.conditions import Key
 
 DYNAMO_BD = os.environ['DYNAMO_BD']
 
-def get_item():
-    dynamo_db = boto3.resource('dynamodb')
+class DynamoAccessor:
+    def __init__(self, dynamo_table):
+        dynamo_db = boto3.resource('dynamodb')
+        self.table = dynamo_db.Table(dynamo_table)
 
-    table = dynamo_db.Table(dynamo_table)
-
-    response = table.get_item(
-        Key={
-            'dni':'46513503'
-        }
-    )
-
-    return response["Items"][0] if any(response["Items"]) else None
+    def get_data_from_dynamo(self, dni):
+        response = self.table.query(KeyConditionExpression=Key('dni').eq(dni))
+        return response["Items"][0] if any(response["Items"]) else None
 
 def lambda_handler(event, context):
-    log.debug("Received event {}".format(json.dumps(event)))
-    return {}
+    dynamo_backend = DynamoAccessor(DYNAMO_BD)
+    db_element = dynamo_backend.get_data_from_dynamo(event['dni'])
+    return db_element
