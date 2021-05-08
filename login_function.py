6c7b609bad8f28e6ca3e6f5efd3b45928b9e6ff1
aaleon20 <aaleon20@outlook.com>
2021-05-07 22:29:38 -0500

    Actualizacion de servicios

--- login_function.py
+++ login_function.py
@@ -4,15 +4,16 @@ from boto3.dynamodb.conditions import Key
 
 DYNAMO_BD = os.environ['DYNAMO_BD']
 
-def get_login(dni, password, dynamodb=None):
-    if not dynamodb:
-       dynamo_db = boto3.resource('dynamodb') 
+class DynamoAccessor:
+    def __init__(self, dynamo_table):
+        dynamo_db = boto3.resource('dynamodb')
+        self.table = dynamo_db.Table(dynamo_table)
 
-    table = dynamo_db.Table(dynamo_table)
+    def get_data_from_dynamo(self, dni):
+        response = self.table.query(KeyConditionExpression=Key('dni').eq(dni))
+        return response["Items"][0] if any(response["Items"]) else None
 
-    try:
-        response = self.table.get_item(Key={'dni':dni, 'password':password})
-    except ClientError as e:
-        print(e.response['Error']['Message']
-    else
-        return response['Item']
+def lambda_handler(event, context):
+    dynamo_backend = DynamoAccessor(DYNAMO_BD)
+    db_element = dynamo_backend.get_data_from_dynamo(event['dni'], event['password'])
+    return db_element
