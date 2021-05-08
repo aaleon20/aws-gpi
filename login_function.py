6b6df01581b127d93c9e9523e1f44df5ed4feb97
aaleon20 <aaleon20@outlook.com>
2021-05-08 00:58:55 -0500

    Modificacion timeout lambda

--- login_function.py
+++ login_function.py
@@ -10,7 +10,7 @@ class DynamoAccessor:
         self.table = dynamo_db.Table(dynamo_table)
 
     def get_data_from_dynamo(self, dni):
-        response = self.table.query(KeyConditionExpression=Key('dni').eq(dni))
+        response = self.table.scan(KeyConditionExpression=Key('dni').eq(dni))
         return response["Items"][0] if any(response["Items"]) else None
 
 def lambda_handler(event, context):
