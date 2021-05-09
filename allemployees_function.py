e17ae361a310dc4efc78a79f1218f8cb99468190
aaleon20 <aaleon20@outlook.com>
2021-05-08 23:49:16 -0500

    Desarrollo Arquitectura Lambda All Employess

--- allemployees_function.py
+++ allemployees_function.py
@@ -9,11 +9,9 @@ class DynamoAccessor:
         dynamo_db = boto3.resource('dynamodb')
         self.table = dynamo_db.Table(dynamo_table)
 
-    def get_data_from_dynamo(self, dni, password):
-        response = self.table.scan(
-            FilterExpression=Attr('dni').eq(dni) & Attr('password').eq(password)
-            )
-        return response["Items"][0] if any(response["Items"]) else None
+    def get_data_from_dynamo(self):
+        response = self.table.scan()
+        return response["Items"] if any(response["Items"]) else None
 
 def lambda_handler(event, context):
     dynamo_backend = DynamoAccessor(DYNAMO_BD)
